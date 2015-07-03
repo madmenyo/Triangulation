133737caad6735243b519ad97a9df064617e3024
madmenyo <m.gouw@live.com>
2015-07-03 10:58:39 +0200

    Blank Project

---
+++ android/src/net/madmenyo/triangulation/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.madmenyo.triangulation.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.madmenyo.triangulation.Triangulation;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Triangulation(), config);
+	}
+}
